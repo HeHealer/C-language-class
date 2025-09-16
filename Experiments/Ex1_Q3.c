6d84e7e2f537d6b9419a4b22c448b69e519cd1c1
Avishi Singhal <165550621+HeHealer@users.noreply.github.com>
2025-09-16 14:26:27 +0530

    Class

--- Experiments/Ex1_Q3.c
+++ Experiments/Ex1_Q3.c
@@ -1,2 +1,13 @@
 //3.Write a program that prompts the user to enter their name and age.
 
+#include<stdio.h>
+int main()
+{
+    char name[30];
+    int age;
+    printf("Enter your name: ");
+    scanf("%s",name);
+    printf("Enter your age: ");
+    scanf("%d",&age);
+    printf("Name: %s\nAge:%d\n",name,age);
+}
