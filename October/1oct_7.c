12c7d1844a4153cc416ca7bfa9c5412bac96d68d
Avishi Singhal <165550621+HeHealer@users.noreply.github.com>
2025-10-01 13:01:56 +0530

    Class

--- October/1oct_7.c
+++ October/1oct_7.c
@@ -7,11 +7,25 @@ struct Employee
     char name[100];
     float basic,da,hra,da_per,hra_per,gross;
 };
-void salary(struct Employee emp)
+void input(struct Employee *emp)
 {
-    emp.da=(emp.da_per/100)*emp.basic;
-    emp.hra=(emp.hra_per/100)*emp.basic;
-    emp.gross=emp.basic+emp.da+emp.hra;
+    printf("Enter Employee ID: ");
+    scanf("%d",&emp->id);
+    getchar();
+    printf("Enter Employee Name: ");
+    fgets(emp->name,sizeof(emp->name),stdin);
+    printf("Enter Basic Salary: ");
+    scanf("%f",&emp->basic);
+    printf("Enter DA percentage: ");
+    scanf("%f",&emp->da_per);
+    printf("Enter HRA Percentage: ");
+    scanf("%f",&emp->hra_per);
+}
+void salary(struct Employee *emp)
+{
+    emp->da=(emp->da_per/100)*emp->basic;
+    emp->hra=(emp->hra_per/100)*emp->basic;
+    emp->gross=emp->basic+emp->da+emp->hra;
 }
 void details(struct Employee emp)
 {
@@ -22,22 +36,9 @@ void details(struct Employee emp)
     printf("HRA: %f\n",emp.hra);
     printf("Gross Salary: %f\n",emp.gross);
 }
-void input(struct Employee emp)
-{
-    printf("Enter Employee ID: ");
-    scanf("%d",&emp.id);
-    getchar();
-    printf("Enter Employee Name: ");
-    fgets(emp.name,sizeof(emp.name),stdin);
-    printf("Enter Basic Salary: ");
-    scanf("%f",&emp.basic);
-    printf("Enter DA percentage: ");
-    scanf("%f",&emp.da);
-    printf("Enter HRA Percentage: ");
-    scanf("%f",&emp.hra);
-}
 int main(){
     struct Employee emp;
-    input(emp);
+    input(&emp);
+    salary(&emp);
     details(emp);
 }
