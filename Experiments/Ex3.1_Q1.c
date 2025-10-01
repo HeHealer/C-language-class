e27f073453ed333067d81a70ded8b7ea0da4357c
Avishi Singhal <165550621+HeHealer@users.noreply.github.com>
2025-10-01 15:25:20 +0530

    Class

--- Experiments/Ex3.1_Q1.c
+++ Experiments/Ex3.1_Q1.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int main(){
     float a,b,c;
-    printf("Enter the lengths of the three sides of the triangle");
+    printf("Enter the lengths of the three sides of the triangle: ");
     scanf("%f%f%f",&a,&b,&c);
     if (a+b>c && a+c>b && b+c>a)
     {
