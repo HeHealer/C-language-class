94d38094e325ffd4f72a0a185ea09b8728439789
Avishi Singhal <165550621+HeHealer@users.noreply.github.com>
2025-10-30 18:40:25 +0530

    Class

---
+++ October/30Oct_Q1.c
@@ -0,0 +1,25 @@
+//WAP to count how many times program is executed and display a message at the beginning of the program.
+
+#include<stdio.h>
+int main(){
+    FILE *fptr;
+    int count=0;
+    fptr=fopen("D:/Pollution.txt","r");
+    if(fptr!=NULL)
+    {
+        fscanf(fptr,"%d",&count);
+        fclose(fptr);
+    }
+    count++;
+    printf("This is the execution number: %d",count);
+    fptr=fopen("D:/Pollution.txt","w");
+    if(fptr!=NULL)
+    {
+        fprintf(fptr,"%d",count);
+        fclose(fptr);
+    }
+    else
+    {
+        printf("Error");
+    }
+}
